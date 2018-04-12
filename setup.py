01a0e88d813bb1d662718d50e62438cccb9fb7ad
kbingham_netfoundry <kenneth.bingham@netfoundry.io>
2018-04-12 12:11:54 -0400

    let deleteNetwork() return true when API status is DELETED or HTTP NOT_FOUND

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     author='NetFoundry',
     author_email='ops-mgmt@netfoundry.io',
     url='https://netfoundry.atlassian.net/wiki/spaces/PUB/blog/2017/10/15/11829509/MOP+for+Python',
-    download_url='https://netfoundry.jfrog.io/netfoundry/list/python/nfmop/1.0.6/nfmop-1.0.6.tar.gz',
+    download_url='https://netfoundry.jfrog.io/netfoundry/list/python/nfmop/1.0.7/nfmop-1.0.7.tar.gz',
     description='API wrappers and configuration for NetFoundry\'s Management Orchestration Platform',
     license='MIT',
 
-    version='1.0.6',
+    version='1.0.7',
     install_requires=[
         'requests >= 2.18.4',
         'pysocks >= 1.6.7',
