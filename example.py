331d34d6315fb58b5b8e15e2f66f945d9bcf452a
kbingham_netfoundry <kenneth.bingham@netfoundry.io>
2020-10-26 10:13:14 -0400

    show progress if creating the example network

--- example.py
+++ example.py
@@ -10,9 +10,11 @@ NetworkGroup = netfoundry.NetworkGroup(Org.token, Org.networkGroupId)
 # create and use a Network
 netName = "BibbidiBobbidiBoo1"
 if netName in NetworkGroup.networksByName.keys():
-    NetworkGroup.waitForStatus("PROVISIONED","network",NetworkGroup.networksByName[netName],wait=999,progress=True)
+    NetworkGroup.waitForStatus("PROVISIONED","network",NetworkGroup.networksByName[netName],wait=999,progress=False)
     Network = netfoundry.Network(Org.token, NetworkGroup.id, networkName=netName)
 else:
-    netId = NetworkGroup.createNetwork(netName,wait=999,progress=False)
+    netId = NetworkGroup.createNetwork(netName,wait=999,progress=True)
     Network = netfoundry.Network(Org.token, NetworkGroup.id, netId)
 print('{} is {}\n'.format(Network.name, Network.status))
+
+# a list of places where Endpoints or Services or both are located
